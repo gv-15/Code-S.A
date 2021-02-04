c8c5df2b492977edfea3052a473ef81fde805b3c
Edurne <edurnesanj@gmail.com>
2021-02-04 12:16:00 +0100

    Contact class done

--- Agenda/Contacto.cs
+++ Agenda/Contacto.cs
@@ -3,12 +3,47 @@
 /// <summary>
 /// Summary description for Class1
 /// </summary>
-public class Class1
+public class Contacto
 {
-	public Class1()
+	public String Nombre;
+	public int Telefono;
+	public String Email;
+
+	public Contacto(String nombre, int telefono, String email)
 	{
-		//
-		// TODO: Add constructor logic here
-		//
+		Nombre = nombre;
+		Telefono = telefono;
+		Email = email;
+	}
+
+	public String getNombre()
+    {
+		return Nombre;
+    }
+
+	public void setNombre(String nombre)
+	{
+		Nombre = nombre;
+	}
+
+
+	public int getTelefono()
+	{
+		return Telefono;
+	}
+
+	public void setTelefono(int telefono)
+	{
+		Telefono = telefono;
+	}
+
+	public String getEmail()
+	{
+		return Email;
+	}
+
+	public void setEmail(String email)
+	{
+		Email = email;
 	}
 }
