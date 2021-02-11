0af61c4eab759ebc38fd0bda28c638a11fcc2fe8
Iker Sacristan <ikersacristan@gmail.com>
2021-02-11 13:03:10 +0100

    create tableColumn structure

--- Database/TableColumn.cs
+++ Database/TableColumn.cs
@@ -6,5 +6,19 @@ namespace Database
 {
     class TableColumn
     {
+        private List<String> Columns;
+        public TableColumn()
+        {
+            Columns = new List<string>();
+        }
+        public List<String> GetColumn()
+        {
+            return Columns;
+        }
+        public void DeleteFrom()
+        { 
+
+        }
+        
     }
 }
