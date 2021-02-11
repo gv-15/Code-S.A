72b51aa9e31837e932dd15323a9e86ad0421592f
Edurne <edurnesanj@gmail.com>
2021-02-11 13:10:52 +0100

    1

--- Database/Table.cs
+++ Database/Table.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Database
 {
     public class Table
     {
+        private List<TableColumn> columns; 
+        public Table()
+        {
+            columns = new List<TableColumn>();
 
+
+        }
+
+        public void AddColumn(TableColumn column)
+        {
+            columns.Add(column);
+        }
     }
 }
