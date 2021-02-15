fa79e74c320c0bee437a6ceb5de973591a1ca4e8
Xabier Barguilla <xabierbarguilla@gmail.com>
2021-02-15 13:51:28 +0200

    Creating the table Unit Test

---
+++ UnitTests/TableUnitTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TableUnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
