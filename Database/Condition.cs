2ce341d064f704cf02966f2ee29a8b4750937f85
Iker Sacristan <ikersacristan@gmail.com>
2021-02-15 12:21:54 +0100

    I create the class Condition

---
+++ Database/Condition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+   public class Condition
+    {
+
+        private Enum Operator;
+        private String Value;
+        private TableColumn ColumnName;
+        public Condition(Enum operation, String value, TableColumn column)
+        {
+            Operator = operation;
+            Value = value;
+            ColumnName = column;
+
+        }
+    }
+}
