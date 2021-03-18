7dd6b9b775ceb6b01a903a1a34cb3c550715d144
Xabier Barguilla <xabierbarguilla@gmail.com>
2021-03-18 12:14:48 +0100

    #23 A first version of TestLoad() has been created. There needs to make sure it works.

--- UnitTests/UnitTestDatabase.cs
+++ UnitTests/UnitTestDatabase.cs
@@ -46,6 +46,37 @@ namespace UnitTests
 
         }
 
+        public void TestLoad()
+        {
+            DB dbLoad = new DB("Loaded Database");
+
+            // Guardar la DB con Save()
+            DB db = new DB("MyDB", "Admin", "SoyAdmin");
+
+            TableColumn tc1 = new TableColumn("NombreAdmin");
+
+            TableColumn tc2 = new TableColumn("EdadAdmin");
+
+            TableColumn tc3 = new TableColumn("PerrosAdmin");
+
+            List<TableColumn> tableColumns = new List<TableColumn>() { tc1, tc2, tc3 };
+
+            Table table = new Table("DatosAdmin", tableColumns);
+
+            table.AddRow(new List<string>() { "Gaizka", "22", "Boss&Drogo" });
+            table.AddRow(new List<string>() { "Edurne", "22", "Zuri" });
+            table.AddRow(new List<string>() { "Iker", "22", "Null" });
+            table.AddRow(new List<string>() { "Xabi", "21", "Null" });
+
+            db.AddTable(table);
+
+            db.Save();
+            //Cargar la DB con Load()
+            dbLoad.Load(System.AppDomain.CurrentDomain.BaseDirectory.ToString(), "MyDB");
+            //Comprobar que se ha cargado correctamente
+
+        }
+
 
 
 
