ec4e3d068847de898278384031647c1469495a3b
Gaizka <gaizkaf1ferrari@gmail.com>
2021-03-28 18:39:39 +0200

    #27 i fix the parte of select where in the parser test

--- UnitTests/UnitTestParser.cs
+++ UnitTests/UnitTestParser.cs
@@ -63,10 +63,10 @@ namespace UnitTests
 
             IQuery query4 = Parser.Parse("SELECT EdadAdmin FROM DatosAdmin WHERE EdadAdmin = 21;");
             Assert.IsTrue(query4 is SelectWhere);
-        
+
             /*
-            string resultadoSelectWhere = "['DatosAdmin']{'Tamara','23','Xia'}";
-            Assert.AreEqual(resultadoSelectWhere, db.RunMiniSqlQuery("SELECT EdadAdmin FROM DatosAdmin WHERE EdadAdmin = 23;"));
+            string resultadoSelectWhere = ""['NombreAdmin','EdadAdmin','PerrosAdmin']{'Gaizka','22','Boss&Drogo'}{'Edurne','22','Zuri'}{'Iker','22','Null'}";
+            Assert.AreEqual(resultadoSelectWhere, db.RunMiniSqlQuery("SELECT EdadAdmin FROM DatosAdmin WHERE EdadAdmin = 22;"));
            
             */
             IQuery query5 = Parser.Parse("DROP TABLE DatosAdmin;");
