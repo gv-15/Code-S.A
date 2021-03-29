e098ec2a3cca7efbc87c4d0e07e0e071514e1da5
Edurne <edurnesanj@gmail.com>
2021-03-29 11:55:32 +0200

    #24 We already have the test of InsertInto in the parser test

--- UnitTests/UnitTestDatabase.cs
+++ UnitTests/UnitTestDatabase.cs
@@ -54,38 +54,7 @@ namespace UnitTests
             Assert.AreEqual(1, num);
             Assert.AreEqual("table3", m_db.GetDBTableList()[0].GetName());
         }
-        /*
-        [TestMethod]
-        public void TestInsert()
-        {
-            m_db = new DB("m_nombre");
-
-            Table people = new Table("people");
-            m_db.AddTable(people);
-
-            TableColumn name = new TableColumn("name");
-            TableColumn surname = new TableColumn("surname");
-            Table t = m_db.GetTable(0);
-            t.AddColumn(name);
-            t.AddColumn(surname);
-
-            List<String> values = new List<string>();
-            values.Add("Adolfo");
-            values.Add("García");
-
-            m_db.InsertInto("people", values);
-
-            Assert.AreEqual(values,people.GetRowByIndex(0));
-            Assert.AreEqual("Adolfo", people.GetColumns()[0].GetColumns()[0]);
-            Assert.AreEqual("García", people.GetColumns()[1].GetColumns()[0]);
-            int i = people.GetColumns()[0].GetColumns().Count;
-            Assert.AreEqual(1, i);
-            int i2 = people.GetColumns()[1].GetColumns().Count;
-            Assert.AreEqual(1, i2);
-
-
-
-        }*/
+        
 
         [TestMethod]
         public void TestSelectWhere()
