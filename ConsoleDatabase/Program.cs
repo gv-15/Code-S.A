d2398033ea2b4e5c2816e09becb51daa6a82564c
Gaizka <gaizkaf1ferrari@gmail.com>
2021-03-30 14:33:30 +0200

    #29 I started making the console program to use

--- ConsoleDatabase/Program.cs
+++ ConsoleDatabase/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Database;
 
 namespace ConsoleDatabase
@@ -7,7 +8,28 @@ namespace ConsoleDatabase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Consola");
+            DB db = new DB("MyDB", "Admin", "SoyAdmin");
+
+            TableColumn tc1 = new TableColumn("NombreAdmin");
+
+            TableColumn tc2 = new TableColumn("EdadAdmin");
+
+            TableColumn tc3 = new TableColumn("PerrosAdmin");
+
+            List<TableColumn> tableColumns = new List<TableColumn>() { tc1, tc2, tc3 };
+
+            Table table = new Table("DatosAdmin", tableColumns);
+
+            table.AddRow(new List<string>() { "Gaizka", "22", "Boss&Drogo" });
+            table.AddRow(new List<string>() { "Edurne", "22", "Zuri" });
+            table.AddRow(new List<string>() { "Iker", "22", "Null" });
+            table.AddRow(new List<string>() { "Xabi", "21", "Null" });
+
+            db.AddTable(table);
+
+            Console.WriteLine(db.RunMiniSqlQuery("SELECT * FROM DatosAdmin; "));
+            
+
         }
     }
 }
