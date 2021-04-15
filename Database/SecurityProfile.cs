779fe55dc6d4c802848ae36fb1b3b46c3ea5d802
Xabier Barguilla <xabierbarguilla@gmail.com>
2021-04-15 13:21:44 +0200

    #34 The security classes and methods have been structured but they have yet to be implemented.

---
+++ Database/SecurityProfile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    public class SecurityProfile
+    {
+        public SecurityProfile()
+        {
+
+        }
+    }
+}
