5d62d03570fdd9127532fb86380fa3a1ccacc548
Xabier Barguilla <xabierbarguilla@gmail.com>
2021-04-19 12:35:45 +0200

    #34 The class security profile has been structured and its methods implemented.

--- Database/SecurityProfile.cs
+++ Database/SecurityProfile.cs
@@ -6,9 +6,58 @@ namespace Database
 {
     public class SecurityProfile
     {
-        public SecurityProfile()
-        {
+        private string m_name;
 
+        private string m_password;
+
+        private List<Priviledge> m_priviledges;
+
+        public SecurityProfile(string name, string password, List<Priviledge> priviledges)
+        {
+            m_name = name;
+            m_password = password;
+            m_priviledges = priviledges;
+        }
+
+        public void AddPriviledge(Priviledge priviledge)
+        {
+            if (!CheckPriviledge(priviledge))
+            {
+                m_priviledges.Add(priviledge);
+            }
+
+        }
+
+        public void RemovePriviledge(Priviledge priviledge)
+        {
+            if (CheckPriviledge(priviledge))
+            {
+                m_priviledges.Remove(priviledge);
+            }
+
+        }
+
+        public bool CheckPriviledge(Priviledge priviledge)
+        {
+            bool found = false;
+            int i = 0;
+            if (m_priviledges.Count!=0)
+            {
+                while (!found && i < m_priviledges.Count)
+                {
+                    if (m_priviledges[i].Equals(priviledge))
+                    {
+                        found = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+
+            }
+
+            return found;
         }
     }
 }
