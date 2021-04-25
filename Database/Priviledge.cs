a90affa2edb7f720cac212a36ecdd8a21fca5caa
Xabier Barguilla <xabierbarguilla@gmail.com>
2021-04-26 00:31:28 +0200

    #34 A first version of Security and its related classes has been updated.

--- Database/Priviledge.cs
+++ Database/Priviledge.cs
@@ -6,12 +6,32 @@ namespace Database
 {
     public class Priviledge
     {
-        public enum priviledge_type { DELETE, INSERT, SELECT, UPDATE};
+        public enum Priviledge_type { DELETE, INSERT, SELECT, UPDATE};
 
         private Enum m_type;
 
+        private string m_tableName;
 
-        public Priviledge(Enum type)
+
+        public Priviledge(Enum type, string tName)
+        {
+            m_type = type;
+            m_tableName = tName;
+        }
+
+        public string GetTableName()
+        {
+            return m_tableName;
+        }
+        public void SetTableName(String name)
+        {
+            m_tableName = name;
+        }
+        public string GetPriviledgeType()
+        {
+            return m_type.ToString();
+        }
+        public void setPriviledgeType(Enum type)
         {
             m_type = type;
         }
