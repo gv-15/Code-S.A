c290d4fb8bec59d627fac0eb26ef70bc51c361f9
Iker <ikersacristan@gmail.com>
2021-04-26 13:20:49 +0200

    #37 I started to do security test

---
+++ UnitTests/UnitTestSecurity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Database;
+
+namespace UnitTests
+{
+   
+    [TestClass]
+    public class UnitTestSecurity
+    {
+        private string name;
+        private string password;
+        private List<Priviledge> priviledges;
+
+        [TestMethod]
+        public void TestGrant()
+        {
+            List<Priviledge> p = new List<Priviledge>() ;
+            SecurityProfile profile = new SecurityProfile("paco", "1234", p  );
+            string profileName = profile.GetName();
+            Table t = new Table("n");
+            string tableName = t.GetName();
+           // Priviledge type = new Priviledge(type.GetPriviledgeType(), tableName);
+        }
+    }
+}
